43da6af2bf3e455e2ac93f4205772a9ad2e68823
Klov Jubani <trustbnbdeveloper@gmail.com>
2025-12-29 22:43:07 +0100

    Fix seedSupabase.js: Add missing Supabase regions (eu-north-1, us-east-2) for pooler discovery

--- scripts/seedSupabase.js
+++ scripts/seedSupabase.js
@@ -135,8 +135,9 @@ async function runMigrationsIfNeeded() {
       }
 
       const regions = [
-        'eu-central-1', 'us-east-1', 'us-west-1', 'us-west-2', 'eu-west-1', 'eu-west-2', 'eu-west-3',
-        'ap-southeast-1', 'ap-southeast-2', 'ap-northeast-1', 'ap-northeast-2', 'sa-east-1', 'ca-central-1', 'ap-south-1'
+        'eu-central-1', 'us-east-1', 'us-west-1', 'us-west-2', 'eu-west-1', 'eu-west-2', 'eu-west-3', 'eu-north-1',
+        'ap-southeast-1', 'ap-southeast-2', 'ap-northeast-1', 'ap-northeast-2', 'sa-east-1', 'ca-central-1', 'ap-south-1',
+        'us-east-2', 'af-south-1', 'me-central-1'
       ];
 
       const dns = await import('dns');
